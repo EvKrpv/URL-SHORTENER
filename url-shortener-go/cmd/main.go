e5b99a13738f97809b3ac44d6741fd77660520b7
Евгений Карпов <evkarpov@MacBook-Air-Evgenij.local>
2026-02-25 05:38:09 +0300

    Initial commit

--- url-shortener-go/cmd/main.go
+++ url-shortener-go/cmd/main.go
@@ -12,32 +12,32 @@ import (
 )
 
 func main() {
-	// load env variagle
+	// Загрузка конфигурации из .env файла и переменных окружения
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//Initialize repository
+	// Инициализация репозитория
 	repo, err := repositories.NewURLRepository(cfg.Postgres)
 	if err != nil {
 		log.Fatalf("Failed to initialize repository: %v", err)
 	}
 
-	// Initialize services
+	// Инициализация сервисов
 	urlService := services.NewURLService(repo, cfg.Redis)
 
-	// Initialize handlers
+	// Инициализация обработчиков
 	urlHandler := handlers.NewURLHandler(urlService)
 
-	// step a router
+	// Инициализация маршрутизатора
 	r := gin.Default()
 
-	// routes
+	// маршруты
 	r.POST("/shorten", urlHandler.CreateURL)
 	r.GET("/:shortCode", urlHandler.GetURL)
 
-	// starting a server
+	// запуск сервера
 	log.Printf("Server running on port %s", cfg.Server.Port)
 	if err := r.Run(":" + cfg.Server.Port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
